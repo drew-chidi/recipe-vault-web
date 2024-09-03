7f07f01167b519dbc12a73e25bc366816f708a38
drew-chidi <chidi.andrew@gmail.com>
2024-09-03 12:50:15 +0100

    integrate pagination feature to my API service

--- hooks/useRecipes.ts
+++ hooks/useRecipes.ts
@@ -1,55 +1,49 @@
-'use client';
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import {
-  fetchRecipes,
-  updateRecipe,
-  deleteRecipe,
-  createRecipe,
-  fetchRecipeById,
-} from '../services/recipeApi';
-import { ApiResponse, RecipeType } from '@/types/recipe.type';
+'use client'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { fetchRecipes, updateRecipe, deleteRecipe, createRecipe, fetchRecipeById } from '../services/recipeApi'
+import { ApiResponse, RecipeType } from '@/types/recipe.type'
 
-export const useGetRecipes = () => {
+export const useGetRecipes = (page?: number) => {
   return useQuery<ApiResponse<RecipeType[]>, Error>({
     queryKey: ['recipes'],
-    queryFn: fetchRecipes,
-  });
-};
+    queryFn: () => fetchRecipes(page),
+  })
+}
 
 export const useGetRecipeById = (id: string) => {
   return useQuery<ApiResponse<RecipeType>, Error>({
     queryKey: ['recipe', id],
     queryFn: () => fetchRecipeById(id),
     enabled: !!id,
-  });
-};
+  })
+}
 
 export const useCreateRecipe = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: (recipe: FormData) => createRecipe(recipe),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['recipes'] });
+      queryClient.invalidateQueries({ queryKey: ['recipes'] })
     },
-  });
-};
+  })
+}
 
 export const useUpdateRecipe = (id: string, recipe: RecipeType) => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: () => updateRecipe(id, recipe),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['recipes'] });
+      queryClient.invalidateQueries({ queryKey: ['recipes'] })
     },
-  });
-};
+  })
+}
 
 export const useDeleteRecipe = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: (id: string) => deleteRecipe(id),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['recipes'] });
+      queryClient.invalidateQueries({ queryKey: ['recipes'] })
     },
-  });
-};
+  })
+}
--- services/recipeApi.ts
+++ services/recipeApi.ts
@@ -1,56 +1,56 @@
-'use client';
-import { ApiResponse, RecipeType } from '@/types/recipe.type';
+'use client'
+import { ApiResponse, RecipeType } from '@/types/recipe.type'
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
-console.log(API_BASE_URL);
+console.log(API_BASE_URL)
 
-export const fetchRecipes = async (): Promise<ApiResponse<RecipeType[]>> => {
-  const response = await fetch(`${API_BASE_URL}`);
+export const fetchRecipes = async (page = 1): Promise<ApiResponse<RecipeType[]>> => {
+  const response = await fetch(`${API_BASE_URL}/?page=${page}`)
   if (!response.ok) {
-    throw new Error('Network response was not ok');
+    throw new Error('Network response was not ok')
   }
-  return response.json();
-};
+  return response.json()
+}
 
 export const fetchRecipeById = async (id: string) => {
-  const response = await fetch(`${API_BASE_URL}/${id}`);
+  const response = await fetch(`${API_BASE_URL}/${id}`)
   if (!response.ok) {
-    throw new Error('Network response was not ok');
+    throw new Error('Network response was not ok')
   }
-  return response.json();
-};
+  return response.json()
+}
 
 export const createRecipe = async (recipe: FormData) => {
   const response = await fetch(`${API_BASE_URL}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(recipe),
-  });
+  })
   if (!response.ok) {
-    throw new Error('Network response was not ok');
+    throw new Error('Network response was not ok')
   }
-  return response.json();
-};
+  return response.json()
+}
 
 export const updateRecipe = async (id: string, recipe: RecipeType) => {
   const response = await fetch(`${API_BASE_URL}/${id}`, {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(recipe),
-  });
+  })
   if (!response.ok) {
-    throw new Error('Network response was not ok');
+    throw new Error('Network response was not ok')
   }
-  return response.json();
-};
+  return response.json()
+}
 
 export const deleteRecipe = async (id: string) => {
   const response = await fetch(`${API_BASE_URL}/${id}`, {
     method: 'DELETE',
-  });
+  })
   if (!response.ok) {
-    throw new Error('Network response was not ok');
+    throw new Error('Network response was not ok')
   }
-  return response.json();
-};
+  return response.json()
+}
